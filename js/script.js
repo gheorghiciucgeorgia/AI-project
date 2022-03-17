c87e2e5ff3e1dc5b6d295f14d35a38bccb9d0939
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2022-03-17 22:11:23 +0200

    our-section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
+//making the carousel banner work
 var slideIndex = 1;
 showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
 // Thumbnail image controls
 function currentSlide(n) {
@@ -12,17 +8,44 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {slideIndex = 1}
+
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+var dot1=document.getElementsByClassName('dot')[0];
+var dot2=document.getElementsByClassName('dot')[1];
+var dot3=document.getElementsByClassName('dot')[2];
+var dot4=document.getElementsByClassName('dot')[3];
+var dot5=document.getElementsByClassName('dot')[4];
+
+
+dot1.onclick=function(){
+  currentSlide(1);
+}
+dot2.onclick=function(){
+  currentSlide(2);
+}
+dot3.onclick=function(){
+  currentSlide(3);
+}
+dot4.onclick=function(){
+  currentSlide(4);
+}
+dot5.onclick=function(){
+  currentSlide(5);
 }
