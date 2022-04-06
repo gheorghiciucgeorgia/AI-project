d33317440d685e4d6179a372f38ed7a1dd9d52e3
gheorghiciucgeorgia <georgia-elena.gheorghiciuc@studen.tuiasi.ro>
2022-04-06 12:40:41 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -61,30 +61,3 @@ $('.slick-next').each(function() {
   $(this).html('<i class="fa-solid fa-circle-arrow-right"></i>');
 });
 
-//navigation script
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
-});
